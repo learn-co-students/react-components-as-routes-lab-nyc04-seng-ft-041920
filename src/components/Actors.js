db010d67705f367cb254b9aacbcbffce2b9cf982
residenttrickster <metkusblackus@gmail.com>
2020-07-22 16:12:49 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,22 @@
+  
 import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const renderActors = () => (
+    actors.map((actor, index) => {
+      return (<div key={index}>
+        <h3>{actor.name}</h3>
+      <p>Movies: {actor.movies.map((movie, index) => <li key={index}>{movie}</li>)}</p>
+      </div>)
+    })
+  )
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { movies } from '../data';
+import { move } from 'superagent';
 
 const Movies = () => {
+
+  const renderMovies = () => (
+    movies.map((movie, index) => {
+      return (<div key={index}>
+        <h3>{movie.title}</h3>
+        <p>How long: {movie.time} minutes</p>
+        <p>Genres: {movie.genres.map((genre, index) => <li key={index}>{genre}</li>)}</p>
+      </div>)
+    })
+  )
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {renderMovies()}
     </div>
   );
 };
 
 export default Movies;
+
