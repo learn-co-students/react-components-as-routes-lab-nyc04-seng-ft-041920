cb9083da42db50d3344a0b5f7496ebd78e9baaa3
senadakadric <skadric@fordham.edu>
2020-07-22 20:16:03 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
+  const renderActors = () => {
+    return actors.map((actor) => {
+      return (
+        <div>
+          <h2>{actor.name}</h2>
+          {renderActorsMovies(actor.movies)}
+        </div>
+      );
+    });
+  };
+
+  const renderActorsMovies = (movies) => {
+    return movies.map((movie) => <ul>{movie}</ul>);
+  };
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
+  const renderMovies = () => {
+    return movies.map((movie) => {
+      return (
+        <div>
+          <h2>{movie.title}</h2>
+          <h2>{movie.time}</h2>
+          <ul>{renderMovieGenres(movie.genres)}</ul>
+        </div>
+      );
+    });
+  };
+
+  const renderMovieGenres = (genres) => {
+    return genres.map((movieGenre) => <ul>{movieGenre}</ul>);
+  };
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {renderMovies()}
     </div>
   );
 };
